57acfec3e42abec9ac4526b9397ad63df0fb6669
Vitor <vi.souza.almeida@protonmail.com>
2022-02-01 13:57:26 -0300

    feat: Create campaign function

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -50,3 +50,41 @@ struct CampaignDetails {
     pub image_link: String,
     pub amount_donated: u64,
 }
+
+    fn create_campaign(
+        program_id: &Pubkey,
+        accounts: &[AccountInfo],
+        instruction_data: &[u8],
+    ) -> ProgramResult {
+        let accounts_iter = &mut accounts.iter();
+        let writing_account = next_account_info(account_iter)?;
+
+        let creator_account = next_account_info(account_iter)?;
+
+        if !creator_account.is_signer {
+            return Err(ProgramError::IncorrectProgramId);
+        }
+
+        if writing_account.owner != program_id {
+            msg!("writing_account isn't owned by program")
+            return Err(ProgramError::IncorrectProgramId)
+        }
+
+        let mut input_data = CampaignDetails::try_from_slice(&instruction_data)
+            .expect("Instruction data serialization didn't worked");
+
+            if input_data.admin != *creator_account.key {
+                msg!("Invalid instruction data");
+                return Err(ProgramError::InvalidInstructionData);
+            }
+
+            let rent_exemption = Rent::get()?.minimum_balance(writing_account.data_len());
+
+            if **writing_account.lamports.borrow() < rent_exemption {
+                msg!("The balance of writing_account should be more then rent_exemption");
+                return Err(ProgramError::InsufficientFunds);
+            }
+            input_data.amount_donate=0;
+            input_data.serialize(&mut &mut writing_account.data.borrow_mut()[..])?;
+        Ok(())
+    }
