91c2e5a27442e75c01707d6213411ce904e4b2cf
Vitor <vi.souza.almeida@protonmail.com>
2022-02-01 13:56:28 -0300

    feat: Entrypoint

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -9,3 +9,34 @@ use solana_program::{
     sysvar::Sysvar,
 }
 use borsh::{BorshDeserialize, BorshSerialize};
+
+fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    if instruction_data.len() == 0 {
+        return Err(ProgramError::InvalidInstructionData);
+    }
+    if instruction_data[0] == 0 {
+        return create_campaign(
+            program_id,
+            accounts,
+            &instruction_data[1..instruction_data.len()],
+        );
+    } else if instruction_data[0] == 1 {
+        return withdraw(
+            program_id,
+            accounts,
+            &instruction_data[1..instruction_data.len()]
+        )
+    } else if instruction_data[0] == 2 {
+        return donate(
+            program_id,
+            accounts,
+            &instruction_data[1..instruction_data.len()]
+        );
+    }
+    msg!("Didn't find the entrypoint required");
+    Err(ProgramError:InvalidInstructionData)
+    }
