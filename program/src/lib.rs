e28095b2fb1cec579792f4be87e8b20d9984314a
Vitor <vi.souza.almeida@protonmail.com>
2022-02-01 13:55:35 -0300

    feat: Import solana program and borsh

---
+++ program/src/lib.rs
@@ -0,0 +1,11 @@
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+    rent::Rent,
+    sysvar::Sysvar,
+}
+use borsh::{BorshDeserialize, BorshSerialize};
